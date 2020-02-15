f90b63b4f9383a269ecb1001c35bfae4763da7c7
Codificando <61086809+Codificando2020@users.noreply.github.com>
2020-02-15 08:41:53 -0600

    Windows Forms

---
+++ Gato/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Gato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 14);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(84, 89);
+            this.btn1.TabIndex = 2;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(102, 14);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(84, 89);
+            this.btn2.TabIndex = 3;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(192, 14);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(84, 89);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(192, 110);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(84, 89);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(102, 110);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(84, 89);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 110);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(84, 89);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(192, 205);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(84, 89);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(102, 205);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(84, 89);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Knicknack", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 205);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(84, 89);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Knicknack", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Turno : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Knicknack", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Ganador :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 330);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Font = new System.Drawing.Font("Knicknack", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gato";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Gato/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gato
+{
+    public partial class Form1 : Form
+    {
+        string Primer_Jugador = "", Segundo_Jugador = ""; short Actual = 1; int Ganar = 0;string Ganador = "";
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var resultado = MessageBox.Show("¿Desea ser 'X' ?","Seleccion",MessageBoxButtons.YesNoCancel,MessageBoxIcon.Information);
+            if (resultado==DialogResult.No)
+            {
+                Turno_Jugador("X", "O");
+                Primer_Jugador = "X";
+                Segundo_Jugador = "O";
+            }
+            else if (resultado == DialogResult.Cancel)
+            {
+                this.Close();
+            }
+            else
+            {
+                Turno_Jugador("O", "X");
+                Primer_Jugador = "O";
+                Segundo_Jugador = "X";
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (btn1.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn1.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (btn2.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn2.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (btn3.Text=="")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn3.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (btn4.Text=="")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn4.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (btn5.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn5.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (btn6.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn6.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (btn7.Text== "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn7.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (btn8.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn8.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (btn9.Text == "")
+            {
+                var Letra = Turno_Jugador(Primer_Jugador, Segundo_Jugador);
+                btn9.Text = Letra.ToString(); Validar_Jugada();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public string Turno_Jugador(string Primer_Jugador,string Segundo_Jugador)
+        {
+            
+            if (Actual==1)
+            {
+                Actual--;
+                label1.Text = "Turno : "+Segundo_Jugador.ToString();
+                return Primer_Jugador;
+            }
+            else
+            {
+                Actual++;
+                label1.Text = "Turno : " + Primer_Jugador.ToString();
+                return Segundo_Jugador;
+            }
+        }
+
+        public void Validar_Jugada()
+        {
+            if (btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X" || btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X" || btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X")
+            {
+                Ganador = "X";
+                Ganar = 1;
+            }
+            else if (btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O" || btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O" || btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O")
+            {
+                Ganador = "O";
+                Ganar = 1;
+            }
+            else if (btn1.Text=="X"&& btn2.Text == "X" && btn3.Text == "X" || btn4.Text == "X" && btn5.Text == "X"&& btn6.Text == "X"|| btn7.Text == "X"&& btn8.Text == "X"&& btn9.Text == "X")
+            {
+                Ganador = "X";
+                Ganar = 1;
+            }
+            else if (btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O" || btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O" || btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O")
+            {
+                Ganador = "O";
+                Ganar = 1;
+            }
+            else if (btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X" || btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X" )
+            {
+                Ganador = "X";
+                Ganar = 1;
+            }
+            else if (btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O" || btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O")
+            {
+                Ganador = "O";
+                Ganar = 1;
+            }
+            else
+            {
+                Empate();
+            }
+            if (Ganar==1)
+            {
+                label2.Text = "Ganador : " + Ganador.ToString();
+                MessageBox.Show("Ganador : " + Ganador.ToString(), "Fin del juego", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                Application.Restart();
+            }
+        }
+        public void Empate()
+        {
+            if (btn1.Text!=""&& btn2.Text != ""&& btn3.Text != ""&& btn4.Text != ""&& btn5.Text != ""&& btn6.Text != ""&& btn7.Text != ""&& btn8.Text != ""&& btn9.Text != "")
+            {
+                MessageBox.Show("Empate!", "Fin del juego", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                Application.Restart();
+            }
+        }
+    }
+}
